f2d48ce8a6cb023e078a718e8ebd73baee26b879
GERnguyen <imkai512@gmail.com>
2025-03-06 08:38:26 +0700

    almost-done

--- script.js
+++ script.js
@@ -2,16 +2,7 @@ const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
-const stock_name = ["Adidas SP", "Adidas Temple", 
-    "Adidas TrioLeafs", "Adidas Mercedes", 
-    "Adidas Biker", "Nike SP1", "Nike SP2", "Nike Jordan", 
-    "Adidas Pickleball1", "Adidas Tennis", "Adidas Pickleball2",
-    "Adidas Summer", "Adidas TrioStrike", "Adidas PureWhite",
-     "Adidas HopShop", "Adidas Peace"];
- 
-const stock_price = [650000, 55000, 32000, 990000, 1200000, 650000, 350000, 500000, 
-    400000, 380000, 410000, 320000, 550000, 300000, 880000, 1300000];
-var cart = [];
+let list_product = [];
 
 if (bar) {
     bar.addEventListener('click', () => {
@@ -25,8 +16,191 @@ if (close) {
     })
 }
 
-var addtocart = document.getElementById("addtocart");
-addtocart.onclick = function() {
-    console.log("Đã thêm vào giỏ hàng");
-    alert("Đã thêm vào giỏ hàng");
+
+const cartIcon = document.querySelector("#lg-bag");
+const cartIconMb = document.querySelector("#lg-bag-mb");
+const cart = document.querySelector(".cart-aside");
+const cartClose = document.querySelector("#cart-close");
+const buyNow = document.querySelector(".btn-buy");
+
+cartIcon.addEventListener("click", () => cart.classList.add("active2"));
+cartClose.addEventListener("click", () => cart.classList.remove("active2"));
+cartIconMb.addEventListener("click", () => cart.classList.add("active2"));
+
+const addCartButtons = document.querySelectorAll(".add-cart");
+addCartButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        const productBox = event.target.closest(".product-box");
+        if (productBox) {
+            addToCart(productBox)
+        }
+        else {
+            const productBox = event.target.closest("#prodetails");
+            if (productBox) {
+            addToCartDetail(productBox)
+            }
+        }
+        cart.classList.add("active2");
+    });
+});
+
+
+const cartContent = document.querySelector(".cart-content");
+const addToCart = productBox => {
+    const productImgSrc = productBox.querySelector("img").src;
+    const productTitle = productBox.querySelector(".product-title").textContent;
+    const productPrice = productBox.querySelector(".price").textContent;
+
+    const cartItems = cartContent.querySelectorAll(".cart-product-title");
+    for (let item of cartItems) {
+        if (item.textContent === productTitle) {
+            alert("This item is already in the cart");
+            return;
+        }
+    }
+
+    const cartBox = document.createElement("div");
+    cartBox.classList.add("cart-box");
+    cartBox.innerHTML = `
+    <img src="${productImgSrc}" class="cart-img">
+                <div class="cart-detail">
+                    <h2 class="cart-product-title">${productTitle}</h2>
+                    <span class="cart-price">${productPrice}</span>
+                    <div class="cart-quantity">
+                        <button id="decrement">-</button>
+                        <span class="number">1</span>
+                        <button id="increment">+</button>
+                    </div>
+                </div>
+                <i class="ri-delete-bin-7-fill cart-remove"></i>
+    `;
+
+    cartContent.appendChild(cartBox);
+
+    cartBox.querySelector(".cart-remove").addEventListener("click", () => {
+        cartBox.remove();
+        updateTotalPrice();
+    });
+
+    cartBox.querySelector(".cart-quantity").addEventListener("click", event => {
+        const numberElement = cartBox.querySelector(".number");
+        const decrementButton = cartBox.querySelector("#decrement");
+        let quantity = numberElement.textContent;
+
+        if (event.target.id === "decrement" && quantity > 1) {
+            quantity--;
+            if (quantity === 1) {
+                decrementButton.style.color = "#999";
+                decrementButton.style.cursor = "not-allowed";
+            }
+        } else if (event.target.id === "increment") {
+            quantity++;
+            decrementButton.style.color = "#333";
+            decrementButton.style.cursor = "pointer";
+        }
+        numberElement.textContent = quantity;
+        updateTotalPrice();
+    });
+
+    updateTotalPrice();
 };
+
+
+const addToCartDetail = productBox => {
+    const productImgSrc = productBox.querySelector(".detail-img").src;
+    const productTitle = productBox.querySelector(".product-title").textContent;
+    const productPrice = productBox.querySelector(".price").textContent;
+
+    const cartItems = cartContent.querySelectorAll(".cart-product-title");
+    for (let item of cartItems) {
+        if (item.textContent === productTitle) {
+            alert("This item is already in the cart");
+            return;
+        }
+    }
+
+    const cartBox = document.createElement("div");
+    cartBox.classList.add("cart-box");
+    cartBox.innerHTML = `
+    <img src="${productImgSrc}" class="cart-img">
+                <div class="cart-detail">
+                    <h2 class="cart-product-title">${productTitle}</h2>
+                    <span class="cart-price">${productPrice}</span>
+                    <div class="cart-quantity">
+                        <button id="decrement">-</button>
+                        <span class="number">1</span>
+                        <button id="increment">+</button>
+                    </div>
+                </div>
+                <i class="ri-delete-bin-7-fill cart-remove"></i>
+    `;
+
+    cartContent.appendChild(cartBox);
+
+    cartBox.querySelector(".cart-remove").addEventListener("click", () => {
+        cartBox.remove();
+        updateTotalPrice();
+    });
+
+    cartBox.querySelector(".cart-quantity").addEventListener("click", event => {
+        const numberElement = cartBox.querySelector(".number");
+        const decrementButton = cartBox.querySelector("#decrement");
+        let quantity = numberElement.textContent;
+
+        if (event.target.id === "decrement" && quantity > 1) {
+            quantity--;
+            if (quantity === 1) {
+                decrementButton.style.color = "#999";
+                decrementButton.style.cursor = "not-allowed";
+            }
+        } else if (event.target.id === "increment") {
+            quantity++;
+            decrementButton.style.color = "#333";
+            decrementButton.style.cursor = "pointer";
+        }
+        numberElement.textContent = quantity;
+        updateTotalPrice();
+    });
+
+    updateTotalPrice();
+};
+
+
+
+const updateTotalPrice = () => {
+    const totalPriceElement = document.querySelector(".total-price");
+    const cartBoxes = cartContent.querySelectorAll(".cart-box");
+    let total = 0;
+    cartBoxes.forEach(cartBox => {
+        const priceElement = cartBox.querySelector(".cart-price");
+        const quantityElement = cartBox.querySelector(".number");
+        const price = priceElement.textContent.replace("₫", "");
+        const quantity = quantityElement.textContent;
+        total += price * quantity;
+    });
+    totalPriceElement.textContent = `${total}.000₫`;
+};
+
+
+buyNow.addEventListener("click", () =>  {
+    alert("Cảm ơn bạn đã mua hàng")
+    document.querySelector(".cart-content").innerHTML = ""; 
+    updateTotalPrice();
+}
+);
+
+
+
+const signIn = document.querySelector("#sign-in-button");
+const signUp = document.querySelector("#sign-up-button");
+
+signIn.addEventListener("click", () => {
+    alert("Đăng nhập thành công");
+});
+
+signUp.addEventListener("click", () => {
+    alert("Đăng ký thành công");
+});
+
+
+
